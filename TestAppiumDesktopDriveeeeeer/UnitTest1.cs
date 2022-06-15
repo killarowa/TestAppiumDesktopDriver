217e564585a4b2cc1775913390d2267915856dd5
Kamen Dimitrov <Kamen Dimitrov@KAMKOM>
2022-06-15 03:26:08 +0300

    Add project files.

---
+++ TestAppiumDesktopDriveeeeeer/UnitTest1.cs
@@ -0,0 +1,81 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace ContactBook.DesktopClientTests
+{
+    public class DesktopTest
+    {
+        private const string AppiumUrl = "http://127.0.0.1:4723/wd/hub";
+        //private const string ContactsBookUrl = "https://contactbook.nakov.repl.co/api";
+        //private const string appLocation = @"C:\Work\ContactBook-DesktopClient\ContactBook-DesktopClient.exe";
+
+        private WindowsDriver<WindowsElement> driver;
+        private AppiumOptions options;
+
+        [SetUp]
+        public void StartApp()
+        {
+            options = new AppiumOptions() { PlatformName = "Windows" };
+            options.AddAdditionalCapability("app", @"C:\Work\ContactBook-DesktopClient\ContactBook-DesktopClient.exe");
+
+            driver = new WindowsDriver<WindowsElement>(new Uri(AppiumUrl), options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+
+        [TearDown]
+        public void CloseApp()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void Test_SearchContact_VerifyFirstResult()
+        {
+            // Arrange
+            var urlField = driver.FindElementByAccessibilityId("textBoxApiUrl");
+            urlField.Clear();
+            urlField.SendKeys(@"C:\Work\ContactBook-DesktopClient\ContactBook-DesktopClient.exe");
+
+            var buttonConnect = driver.FindElementByAccessibilityId("buttonConnect");
+            buttonConnect.Click();
+
+            string windowsName = driver.WindowHandles[0];
+            driver.SwitchTo().Window(windowsName);
+
+            var editTextField = driver.FindElementByAccessibilityId("textBoxSearch");
+            editTextField.SendKeys("steve");
+
+            //Act
+            var buttonSearch = driver.FindElementByAccessibilityId("buttonSearch");
+            buttonSearch.Click();
+
+            //Case1:
+            // Thread.Sleep(2000);
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+            var element = wait.Until(d =>
+            {
+                var searchLabel = driver.FindElementByAccessibilityId("labelResult").Text;
+                return searchLabel.StartsWith("Contacts found");
+            });
+
+            var searchLabel = driver.FindElementByAccessibilityId("labelResult").Text;
+            Assert.That(searchLabel, Is.EqualTo("Contacts found: 1"));
+
+            // Assert
+            var firstName = driver.FindElement(By.XPath("//Edit[@Name=\"FirstName Row 0, Not sorted.\"]"));
+            var lastName = driver.FindElement(By.XPath("//Edit[@Name=\"LastName Row 0, Not sorted.\"]"));
+
+            Assert.That(firstName.Text, Is.EqualTo("Steve"));
+            Assert.That(lastName.Text, Is.EqualTo("Jobs"));
+        }
+    }
+}
